695910c8a9e9c1278d64c5ae795943d49315f6f7
Zorg <754543850@qq.com>
2020-06-05 03:37:36 +0800

    菜单加载

--- src/permission.js
+++ src/permission.js
@@ -27,27 +27,21 @@ router.beforeEach(async(to, from, next) => {
       NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
       // determine whether the user has obtained his permission roles through getInfo
-      const hasRoles = store.getters.roles && store.getters.roles.length > 0
-      if (hasRoles) {
+      if (store.getters.permissions && store.getters.permissions.length > 0) {
         next()
       } else {
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          console.log("111")
           await store.dispatch('user/getInfo')
-          console.log("222")
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes')
-          console.log("333", accessRoutes)
           // dynamically add accessible routes
           router.addRoutes(accessRoutes)
-          console.log("444")
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
           next({ ...to, replace: true })
         } catch (error) {
-          console.log("出错了")
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -30,15 +30,37 @@ function hasPermission(permissions, route) {
  * @param permissions
  */
 export function filterAsyncRoutes(routes) {
+  // return routes.filter(route => {
+  //   if (route.component) {
+  //     // Layout组件特殊处理
+  //     if (route.component === 'Layout') {
+  //       route.component = Layout
+  //     } else {
+  //       route.component = loadView(route.component)
+  //     }
+  //   }
+  //   if (route.children != null && route.children && route.children.length) {
+  //     route.children = filterAsyncRoutes(route.children)
+  //   }
+  //   const meta = {};
+  //   meta.title = route.title;
+  //   meta.icon = route.icon;
+  //   route.meta = meta;
+  //   return true
+  // })
   const res = []
   routes.forEach(route => {
-    console.log(route)
     // const tmp = { ...route }
     const tmp = {}
     const meta = {};
     meta.title = route.title;
     meta.icon = route.icon;
-    tmp.path = route.path;
+    if (route.path) {
+      tmp.path = route.path;
+    } else {
+      tmp.path = "/404";
+    }
+
     if (route.component) {
       tmp.component = loadView(route.component);
     } else {
@@ -73,9 +95,11 @@ const actions = {
   generateRoutes({ commit }) {
     return new Promise(resolve => {
       getRouters().then(res => {
-        // let accessedRoutes = filterAsyncRoutes(res.data)
-        let accessedRoutes = asyncRoutes;
-        // accessedRoutes.push({ path: '*', redirect: '/404', hidden: true })
+        let accessedRoutes = filterAsyncRoutes(res.data)
+        let accessedRoutes2 = asyncRoutes;
+        console.log(accessedRoutes);
+        console.log(accessedRoutes2);
+        accessedRoutes.push({ path: '*', redirect: '/404', hidden: true })
         commit('SET_ROUTES', accessedRoutes)
         resolve(accessedRoutes)
       });
